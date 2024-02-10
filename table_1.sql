aca4c9997295ab29d05ff8b54aa5152247febdba
Kashkarov Pavel <72447188+pavelkashkarov@users.noreply.github.com>
2024-02-10 22:12:26 +0300

    Create table_1.sql

---
+++ table_1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE PERSONS (
+    name VARCHAR(255),
+    surname VARCHAR(255),
+    age INT,
+    phone_number VARCHAR(20),
+    city_of_living VARCHAR(255),
+    PRIMARY KEY (name, surname, age)
+);
